8405cb015b5fc9645d58943f70e5e512be0c360a
HYOJIN1024 <128892314+HYOJIN1024@users.noreply.github.com>
2023-03-25 18:34:57 +0900

    Update streamlit_app.py
    added 2nd menu
    added 3rd menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 2 & Bluberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Bolied Free-Range Egg')
